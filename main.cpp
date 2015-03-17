57c820124e1b76f5543aa5274b449aba4195abc5
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2015-03-16 23:28:54 -0400

    Added dependency to GLEW.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 /// \author Patrick Clément-Bonhomme <patrick.cb@gmail.com>
 ///
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <cstdlib>
 
 /// Callback for errors logged by GLFW
 static void glfwErrorCallback(int error, const char* description)
 {
-    std::cerr << "Error " << error << ": " << *description << std::endl;
+    std::cerr << "Error " << error << ": " << description << std::endl;
 }
 
 /// Callback for keys logged by GLFW
@@ -29,21 +31,33 @@ int main(int argc, char* argv[])
 
     if (!glfwInit())
     {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
         return EXIT_FAILURE;
     }
 
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello GLFW3", nullptr, nullptr);
-
     if (window == nullptr)
     {
-        glfwTerminate();    
+        glfwTerminate();
     }
 
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);    
 
+    // Reminder: GLEw requires an OpenGL context before initialization.
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(err) << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "Version: " << glGetString(GL_VERSION)  << std::endl;
+
     glfwSetKeyCallback(window, glfwKeyCallback);
-    
+
     while (!glfwWindowShouldClose(window))
     {
 
