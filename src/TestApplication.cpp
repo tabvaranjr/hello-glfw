7a94f77b95b4972eb86ef8490b93f068bd0645cb
Patrick Clément-Bonhomme <patrick.cb@gmail.com>
2016-11-27 12:17:57 -0500

    Moved demo-specific functions to TestApplication

--- src/TestApplication.cpp
+++ src/TestApplication.cpp
@@ -1,17 +1,67 @@
 #include "TestApplication.h"
 
+#include <GLFW/glfw3.h>
 #include <chrono>
-#include <thread>
+#include <fmt/format.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/vec3.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <GLFW/glfw3.h>
+#include <thread>
 
-#include "Demo.h"
 #include "Camera.h"
+#include "File.h"
 #include "Mesh.h"
 #include "ShaderProgram.h"
+
+std::shared_ptr<ShaderProgram> makeShaderProgram(const std::string& program)
+{
+    auto vertexShaderFile = File::readTextFile(fmt::format("shaders/{0}.vert", program));
+    Shader vs(Shader::Type::Vertex, vertexShaderFile);
+
+    auto geometryShaderFile = File::readTextFile(fmt::format("shaders/{0}.geom", program));
+    Shader gs(Shader::Type::Geometry, geometryShaderFile);
+
+    auto fragmentShaderFile = File::readTextFile(fmt::format("shaders/{0}.frag", program));
+    Shader fs(Shader::Type::Fragment, fragmentShaderFile);
+
+    return std::make_shared<ShaderProgram>(vs, gs, fs);
+}
+
+std::shared_ptr<Mesh> makeSimpleQuad()
+{
+    std::vector<glm::vec3> vertices
+    {
+            { -0.5f, -0.5f, 0.0f },
+            { 0.5f, 0.5f, 0.0f },
+            { -0.5f, 0.5f, 0.0f },
+            { 0.5f, -0.5f, 0.0f },
+            { 0.5f, 0.5f, 0.0f },
+            { -0.5f, -0.5f, 0.0f }
+    };
+
+    std::vector<glm::vec3> colors
+    {
+            { 1.0f, 0.0f, 0.0f },
+            { 0.0f, 1.0f, 0.0f },
+            { 0.0f, 0.0f, 1.0f },
+            { 1.0f, 1.0f, 0.0f },
+            { 0.0f, 1.0f, 0.0f },
+            { 1.0f, 0.0f, 0.0f }
+    };
+
+    std::vector<glm::vec3> normals
+    {
+            { 0.0f, 0.0f, 1.0f },
+            { 0.0f, 0.0f, 1.0f },
+            { 0.0f, 0.0f, 1.0f },
+            { 0.0f, 0.0f, 1.0f },
+            { 0.0f, 0.0f, 1.0f },
+            { 0.0f, 0.0f, 1.0f }
+    };
+
+    return std::make_shared<Mesh>(vertices, colors, normals);
+}
 
 TestApplication::TestApplication(const ApplicationParameters& parameters)
 {
